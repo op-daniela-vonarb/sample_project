ea99e16d2799a7599d0a144abd4338b9f27d182c
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2023-02-06 11:29:53 +1300

    keep old data in form

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -18,7 +18,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="company"
+                name="company" value="{{old('company')}}"
             />
             @error('company')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -33,7 +33,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="title"
+                name="title" value="{{old('title')}}"
                 placeholder="Example: Senior Laravel Developer"
             />
             @error('title')
@@ -50,7 +50,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="location"
+                name="location" value="{{old('location')}}"
                 placeholder="Example: Remote, Boston MA, etc"
             />
             @error('location')
@@ -65,7 +65,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="email"
+                name="email" value="{{old('email')}}"
             />
             @error('email')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -82,7 +82,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="website"
+                name="website" value="{{old('website')}}"
             />
             @error('website')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -96,7 +96,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="tags"
+                name="tags" value="{{old('tags')}}"
                 placeholder="Example: Laravel, Backend, Postgres, etc"
             />
             @error('tags')
@@ -124,7 +124,7 @@
             </label>
             <textarea
                 class="border border-gray-200 rounded p-2 w-full"
-                name="description"
+                name="description" value="{{old('description')}}"
                 rows="10"
                 placeholder="Include tasks, requirements, salary, etc"
             ></textarea>
