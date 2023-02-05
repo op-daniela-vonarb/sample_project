bfef0dae1c97721e3298c798bb26c7b9a1aed57d
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2023-02-06 10:45:32 +1300

    create store controller

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
@@ -26,5 +27,22 @@ class ListingController extends Controller
     // show create form
     public function create() {
         return view('listings.create');
+    }
+
+    // store listing data
+    public function store(Request $request) {
+        // dd($request->all());
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        return redirect('/');
+        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,8 @@ Route::get('/', [ListingController::class, 'index']);
 // show create form
 Route::get('/listings/create', [ListingController::class, 'create']);
 
+// store listing data
+Route::post('/listings', [ListingController::class, 'store']);
+
 // single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
