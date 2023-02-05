0787c827966e8ba1a4fcb9c50df9ed909a704f48
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2023-02-05 14:16:07 +1300

    create listing factory

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,30 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'this is the description'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'this is the description'
-            ]
-            ];
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    }
-
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
