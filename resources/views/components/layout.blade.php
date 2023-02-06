46a4b456171d76428398f7a0422df0dc9af232b9
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2023-02-06 20:05:24 +1300

    create logout functionality

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -45,6 +45,13 @@
                         ><i class="fa-solid fa-gear"></i>
                         Manage Listings</a>
                 </li>
+                <li>
+                    <form class="inline" method="POST" action="/logout">
+                        @csrf
+                        <button type="submit">
+                            <i class="fa-solid fa-door-closed"></i> Logout
+                    </form>
+                </li>
 
                 @else
                 <li>
--- routes/web.php
+++ routes/web.php
@@ -54,3 +54,6 @@ Route::get('/register', [UserController::class, 'create']);
 
 // create new user
 Route::post('/users', [UserController::class, 'store']);
+
+// logout user
+Route::post('/logout', [UserController::class, 'logout']);
