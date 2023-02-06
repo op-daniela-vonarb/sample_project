f4c68805e2db07b9cd8a81997f41eadd10ca3316
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2023-02-06 16:38:58 +1300

    create update functionality

--- routes/web.php
+++ routes/web.php
@@ -36,5 +36,11 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 // store listing data
 Route::post('/listings', [ListingController::class, 'store']);
 
+// show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
 // single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
